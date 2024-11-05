4209110bc0a1bd27d33b6b75ce0eb7c4a007e93a
Creed1211 <125995245+Creeeeeed1211@users.noreply.github.com>
2024-11-05 12:16:40 +0000

    Update helpers.cpp

--- CCCw1/triangles-test/helpers.cpp
+++ CCCw1/triangles-test/helpers.cpp
@@ -2,6 +2,7 @@
 
 #include "../draw2d/color.hpp"
 #include "../draw2d/surface.hpp"
+#include <cmath> 
 
 
 ColorU8_sRGB find_most_red_pixel( Surface const& aSurface )
@@ -31,6 +32,23 @@ ColorU8_sRGB find_most_red_pixel( Surface const& aSurface )
 
 	return ret;
 }
+
+
+ColorU8_sRGB get_pixel_color(Surface const& surface, Surface::Index x, Surface::Index y)
+{
+	auto width = surface.get_width();
+	const std::uint8_t* surfaceData = surface.get_surface_ptr();
+
+
+	Surface::Index index = y * width + x;
+
+
+	const std::uint8_t* pixel = &surfaceData[index * 4];
+
+
+	return { pixel[0], pixel[1], pixel[2] };
+}
+
 ColorU8_sRGB find_least_red_nonzero_pixel( Surface const& aSurface )
 {
 	ColorU8_sRGB ret{ 255, 255, 255 };
@@ -57,5 +75,24 @@ ColorU8_sRGB find_least_red_nonzero_pixel( Surface const& aSurface )
 
 	return ret;
 
+}
+
+
+//bool colors_are_approximately_equal(const coloru8_srgb& color1, const coloru8_srgb& color2, float tolerance = 0.1f)
+//{
+//	// compare each channel with a specified tolerance
+//	return (std::fabs(color1.r - color2.r) <= tolerance * 255) &&
+//		(std::fabs(color1.g - color2.g) <= tolerance * 255) &&
+//		(std::fabs(color1.b - color2.b) <= tolerance * 255);
+//}
+
+
+ColorU8_sRGB interpolate_colors(const ColorU8_sRGB& c1, const ColorU8_sRGB& c2, float t)
+{
+	return {
+		static_cast<std::uint8_t>(c1.r + t * (c2.r - c1.r)),
+		static_cast<std::uint8_t>(c1.g + t * (c2.g - c1.g)),
+		static_cast<std::uint8_t>(c1.b + t * (c2.b - c1.b))
+	};
 }
 
