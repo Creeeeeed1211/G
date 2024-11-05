903fbb8ff99bd1de10d0d9a7cddeb96f150b0de0
Creed1211 <125995245+Creeeeeed1211@users.noreply.github.com>
2024-11-05 12:14:45 +0000

    Update scenario-3.cpp

--- CCCw1/lines-test/scenario-3.cpp
+++ CCCw1/lines-test/scenario-3.cpp
@@ -1,3 +1,99 @@
 #include <catch2/catch_amalgamated.hpp>
+#include "../draw2d/surface.hpp"
+#include "../draw2d/draw.hpp"
+#include "../lines-test/helpers.hpp"
 
-//TODO
+
+
+TEST_CASE("Lines with both points outside the surface", "[OUTSIDE]")
+{
+    Surface surface(200, 200);
+    surface.clear();
+    ColorU8_sRGB color = { 255, 255, 255 };
+
+    SECTION("Horizontal left")
+    {
+        bool pass = true;
+        draw_line_solid(surface, { -100.f, 50.f }, { -50.f, 50.f }, color);
+
+        for (Surface::Index y = 0; y < surface.get_height(); ++y)
+        {
+            for (Surface::Index x = 0; x < surface.get_width(); ++x)
+            {
+                ColorU8_sRGB check = get_pixel_color(surface, x, y);
+                if (check.r != 0 || check.g != 0 || check.b != 0)
+                {
+                    pass = false;
+                    break;
+                }
+            }
+            if (!pass) break;
+        }
+        REQUIRE(pass);
+    }
+
+    SECTION("Vertical top")
+    {
+        bool pass = true;
+        draw_line_solid(surface, { 100.f, 250.f }, { 100.f, 300.f }, color);
+
+        for (Surface::Index y = 0; y < surface.get_height(); ++y)
+        {
+            for (Surface::Index x = 0; x < surface.get_width(); ++x)
+            {
+                ColorU8_sRGB check = get_pixel_color(surface, x, y);
+                if (check.r != 0 || check.g != 0 || check.b != 0)
+                {
+                    pass = false;
+                    break;
+                }
+            }
+            if (!pass) break;
+        }
+        REQUIRE(pass);
+    }
+
+    SECTION("Diagonal bottom-left")
+    {
+        bool pass = true;
+        draw_line_solid(surface, { -50.f, -50.f }, { -10.f, -10.f }, color);
+
+        for (Surface::Index y = 0; y < surface.get_height(); ++y)
+        {
+            for (Surface::Index x = 0; x < surface.get_width(); ++x)
+            {
+                ColorU8_sRGB check = get_pixel_color(surface, x, y);
+                if (check.r != 0 || check.g != 0 || check.b != 0)
+                {
+                    pass = false;
+                    break;
+                }
+            }
+            if (!pass) break;
+        }
+        REQUIRE(pass);
+    }
+
+    SECTION("Diagonal top-right")
+    {
+        bool pass = true;
+        draw_line_solid(surface, { 250.f, 250.f }, { 300.f, 300.f }, color);
+
+        for (Surface::Index y = 0; y < surface.get_height(); ++y)
+        {
+            for (Surface::Index x = 0; x < surface.get_width(); ++x)
+            {
+                ColorU8_sRGB check = get_pixel_color(surface, x, y);
+                if (check.r != 0 || check.g != 0 || check.b != 0)
+                {
+                    pass = false;
+                    break;
+                }
+            }
+            if (!pass) break;
+        }
+        REQUIRE(pass);
+    }
+
+   
+}
