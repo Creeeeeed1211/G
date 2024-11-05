c5d2948c78a59e05c2de775132db4f69fc9fa02f
Creed1211 <125995245+Creeeeeed1211@users.noreply.github.com>
2024-11-05 12:17:03 +0000

    Update helpers.hpp

--- CCCw1/triangles-test/helpers.hpp
+++ CCCw1/triangles-test/helpers.hpp
@@ -2,9 +2,18 @@
 #define HELPERS_HPP_DD37133A_D9CE_4998_AA48_41DA09E1517C
 
 #include "../draw2d/forward.hpp"
+#include "../draw2d/surface.hpp"
+#include "../draw2d/draw.hpp"
+#include <cmath>
 
 
 ColorU8_sRGB find_most_red_pixel( Surface const& );
 ColorU8_sRGB find_least_red_nonzero_pixel( Surface const& );
+ColorU8_sRGB get_pixel_color(Surface const& surface, Surface::Index x, Surface::Index y);
+// Declaration with a default argument
+//bool colors_are_approximately_equal(ColorU8_sRGB color1, ColorU8_sRGB color2, int tolerance = 5);
+//bool colors_are_approximately_equal(const ColorU8_sRGB& color1, const ColorU8_sRGB& color2, float tolerance = 0.1f);
+
+ColorU8_sRGB interpolate_colors(const ColorU8_sRGB& c1, const ColorU8_sRGB& c2, float t);
 
 #endif // HELPERS_HPP_DD37133A_D9CE_4998_AA48_41DA09E1517C
