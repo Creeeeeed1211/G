7a7a85d4e08f567e856d9e6329d317ce109bbd8a
Creed1211 <125995245+Creeeeeed1211@users.noreply.github.com>
2024-11-05 12:14:12 +0000

    Update scenario-1.cpp

--- CCCw1/lines-test/scenario-1.cpp
+++ CCCw1/lines-test/scenario-1.cpp
@@ -1,3 +1,48 @@
 #include <catch2/catch_amalgamated.hpp>
+#include "../draw2d/surface.hpp"
+#include "../draw2d/draw.hpp"
 
-//TODO
+TEST_CASE( "identical", "[identical]" )
+{
+    
+    Surface surface1(200, 200);
+    Surface surface2(200, 200);
+
+    const Surface::Index startX = 20, startY = 50;
+    const Surface::Index endX = 150, endY = 180;
+
+   
+    const ColorU8_sRGB color = {255, 255, 255};
+
+    //opposite directions
+    surface1.clear();
+    surface2.clear();
+    draw_line_solid(surface1, {static_cast<float>(startX), static_cast<float>(startY)},
+                    {static_cast<float>(endX), static_cast<float>(endY)}, color);
+    draw_line_solid(surface2, {static_cast<float>(endX), static_cast<float>(endY)},
+                    {static_cast<float>(startX), static_cast<float>(startY)}, color);
+
+
+    
+    bool identical = true;
+    
+    for (Surface::Index y = 0; y < surface1.get_height(); y++)
+    {
+        for (Surface::Index x = 0; x < surface1.get_width(); x++)
+        {
+            Surface::Index index1 = surface1.get_linear_index(x, y);
+            Surface::Index index2 = surface2.get_linear_index(x, y);
+
+            if (surface1.get_surface_ptr()[index1] != surface2.get_surface_ptr()[index2])
+            {
+                identical = false;
+                break;
+            }
+        }
+        if (!identical) break;
+    }
+
+    REQUIRE(identical);
+
+   
+}
