0de8b84ba914dbf0cc106b975d94bc8b5c161b01
Creed1211 <125995245+Creeeeeed1211@users.noreply.github.com>
2024-11-06 23:47:31 +0000

    Update helpers.hpp

--- CCCw1/lines-test/helpers.hpp
+++ CCCw1/lines-test/helpers.hpp
@@ -1,18 +1,14 @@
-#ifndef HELPERS_HPP_D7CD1385_1BA6_4B6B_9F31_72B9B8A05AA7
-#define HELPERS_HPP_D7CD1385_1BA6_4B6B_9F31_72B9B8A05AA7
+#ifndef HELPERS_HPP_DD37133A_D9CE_4998_AA48_41DA09E1517C
+#define HELPERS_HPP_DD37133A_D9CE_4998_AA48_41DA09E1517C
 
-#include <array>
-
-#include <cstddef>
+#include "../draw2d/forward.hpp"
 #include "../draw2d/surface.hpp"
 #include "../draw2d/draw.hpp"
-#include "../draw2d/forward.hpp"
+#include <cmath>
 
 
-std::size_t max_row_pixel_count( Surface const& );
-std::size_t max_col_pixel_count( Surface const& );
+ColorU8_sRGB find_most_red_pixel( Surface const& );
+ColorU8_sRGB find_least_red_nonzero_pixel( Surface const& );
 ColorU8_sRGB get_pixel_color(Surface const& surface, Surface::Index x, Surface::Index y);
 
-std::array<std::size_t,9> count_pixel_neighbours( Surface const& );
-
-#endif // HELPERS_HPP_D7CD1385_1BA6_4B6B_9F31_72B9B8A05AA7
+#endif // HELPERS_HPP_DD37133A_D9CE_4998_AA48_41DA09E1517C
